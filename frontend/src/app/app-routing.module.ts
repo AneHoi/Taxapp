b906a143b29869ab3462825b7c9681531a8d13a7
aneHoimark <anehoimark@gmail.com>
2023-11-09 11:56:27 +0100

    frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './login/login.page';
 
 const routes: Routes = [
   {
@@ -11,6 +12,15 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  }
+
 ];
 
 @NgModule({
--- frontend/src/app/home/home-routing.module.ts
+++ frontend/src/app/home/home-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { LoginPage } from '../login/login.page';
 
 const routes: Routes = [
   {
