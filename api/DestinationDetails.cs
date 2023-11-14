f9751d9ceb8c8b516a13b13e6eb3c71a740f893b
hjorthen1243 <114651351+hjorthen1243@users.noreply.github.com>
2023-11-14 10:35:32 +0100

    Api key test

---
+++ api/DestinationDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+public class DeastinationDetalis
+{
+    static async Task Main()
+    {
+        string apiKey = "";
+        string startAddress = "";
+        string endAddress = "";
+
+        var startLocation = await GetLocationAsync(startAddress, apiKey);
+        var endLocation = await GetLocationAsync(endAddress, apiKey);
+
+        var directions = await GetDirectionsAsync(startLocation, endLocation, apiKey);
+     }
+
+    static async Task<JObject> GetLocationAsync(string address, string apiKey)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            var response = await httpClient.GetStringAsync($"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={apiKey}");
+            return JObject.Parse(response);
+        }
+    }
+
+    static async Task<JObject> GetDirectionsAsync(JObject origin, JObject destination, string apiKey)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            var response = await httpClient.GetStringAsync($"https://maps.googleapis.com/maps/api/directions/json?origin={origin["lat"]},{origin["lng"]}&destination={destination["lat"]},{destination["lng"]}&key={apiKey}");
+            return JObject.Parse(response);
+        }
+    }
+}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import {GOOGLEAPIKEY} from "../../../api/ApiKey/GoogleApiKey";
+
+
+declare var google: any;
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
+  templateUrl: `
+    <html lang='en'>
+<head>
+  <meta charset="utf-8">
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src= "https://maps.googleapis.com/maps/api/js?key=" + GOOGLEAPIKEY + "&libraries=places" ></script>
+</head>
+<body>
+  <app-root></app-root>
+</body>
+</html>
+  `,
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  @ViewChild('mapContainer') mapContainer: ElementRef;
+
+  private  map: any;
+  private directionService: any;
+  private directionDisplay: any;
+  constructor(private ngzone: NgZone) {}
+
+  ngOnInit(){
+    this.initializeMap();
+  }
+
+  private initializeMap(){
+    this.map = new google.map.Map(this.mapContainer.nativeElement, {
+      center: {lat: 0, lng: 0},
+      zoom: 2,
+    });
+
+    this.directionService = new google.maps.DirectionService();
+    this.directionDisplay = new google.maps.DirectionsRenderer();
+    this.directionDisplay.setMap(this.map);
+  }
+
+  showRoute() {
+    const startLocation = 'Your Starting Address';
+    const endLocation = 'Your Ending Address';
+
+    const request = {
+      origin: startLocation,
+      destination: endLocation,
+      travelMode: google.maps.TravelMode.DRIVING,
+    };
+
+    this.directionService.route(request, (result: any, status: any) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        this.ngzone.run(() => {
+          this.directionDisplay.setDirections(result);
+        });
+      } else {
+        console.error('Directions request failed:', status);
+      }
+    });
+  }
 }
