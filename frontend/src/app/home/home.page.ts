22ebfc2c9d3c66736dd54e3fd0e4bd9a87ae94fe
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-11-27 10:37:56 +0100

    Can now change number of persons

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,29 +1,31 @@
 import {Component} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {State} from 'src/state';
-import { firstValueFrom } from 'rxjs';
-import {TaxiFare, ResponseDto, TaxiPricesDto } from 'src/models'
+import {firstValueFrom} from 'rxjs';
+import {TaxiFare, ResponseDto, TaxiPricesDto} from 'src/models'
+import {environment} from 'src/environments/environment';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  date: string | undefined;
-  datetimeValue: string | undefined;
+    date: string | undefined;
+    datetimeValue: string | undefined;
+    persons: number | undefined;
 
 
-  constructor(public state: State, public http: HttpClient) {
-  }
+    constructor(public state: State, public http: HttpClient) {
+    }
 
-  async searchForPrices() {
-   const result = await firstValueFrom(this.http.get<ResponseDto<TaxiPricesDto>>("http://localhost:5081/TaxaApis/GetTaxaPrices/5,10,1"))
-   this.state.TaxiPrices = result.responseData!;
+    async searchForPrices(km: number, min: number, persons: number | undefined) {
+        const result = await firstValueFrom(this.http.get<ResponseDto<TaxiPricesDto>>(environment.baseURL + "/TaxaApis/GetTaxaPrices/" + km + "," + min + "," + persons))
+        this.state.TaxiPrices = result.responseData!;
 
-  }
+    }
 
-  clickedCard(taxiPrice: any) {
+    clickedCard(taxiPrice: any) {
 
-  }
+    }
 }
