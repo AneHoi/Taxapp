a61ae81cf5b994119012cf4572a3a304a7a2f650
Alex Uldahl Pedersen <41024316+uldahlalex@users.noreply.github.com>
2023-11-30 13:33:40 +0100

    Google Maps til jeres app (#24)
    * files
    
    * prep
    
    * yes
    
    * formatting

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {ActivatedRoute, ActivatedRouteSnapshot, RouteReuseStrategy, RouterModule, Routes} from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientModule, provideHttpClient, withJsonpSupport} from "@angular/common/http";
+import {MapsComponent} from "./maps/maps.component";
+import {HomePage} from "./home/home.page";
+import {LoginPage} from "./login/login.page";
+import {FormsModule} from "@angular/forms";
+import {GoogleMapsModule} from "@angular/google-maps";
+import {CommonModule} from "@angular/common";
+const routes: Routes = [
+
+   {
+      component: HomePage,
+      path: 'home'
+    },
+    {
+      component: LoginPage,
+      path: 'login'
+    }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  declarations: [AppComponent, MapsComponent, HomePage, LoginPage],
+  imports: [RouterModule.forRoot(routes),GoogleMapsModule, CommonModule, RouterModule,  BrowserModule, IonicModule.forRoot({mode: 'ios'}), HttpClientModule, FormsModule],
+  providers: [ provideHttpClient(withJsonpSupport())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ frontend/src/app/maps/maps.component.ts
@@ -0,0 +1,32 @@
+import {Component} from "@angular/core";
+import {Apikey} from "./apikey";
+import {firstValueFrom} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+@Component({
+  template: `<google-map *ngIf="apiLoaded"
+                  height="400px"
+                  width="750px"
+                  [center]="center"
+                  [zoom]="zoom"
+                  (mapClick)="addMarker($event)">
+          <map-marker *ngFor="let position of markerPositions" [position]="position" [options]="markerOptions"/>
+      </google-map>`,
+  selector: 'app-maps',
+})
+export class MapsComponent {
+  apiLoaded = false;
+  constructor(private httpClient: HttpClient) { this.loadMap() }
+  async loadMap() {
+    await firstValueFrom(this.httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=' + Apikey, 'callback'));
+    this.apiLoaded = true;
+  }
+  center: google.maps.LatLngLiteral = {lat: 55.48773485445012, lng: 8.446890874949103}; //her er easv fx
+  zoom = 16;
+  markerOptions: google.maps.MarkerOptions = {draggable: false};
+  markerPositions: google.maps.LatLngLiteral[] = [];
+
+  addMarker(event: google.maps.MapMouseEvent) {
+    this.markerPositions.push(event.latLng!.toJSON());
+    console.log("hey taxapp, der er nu en marker på kortet")
+  }
+}
