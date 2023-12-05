87c249da0eb9cc544f2f4cf291b085d9d1c9af37
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-12-05 12:39:40 +0100

    Test fixing branch (#29)
    * Test are setup
    
    * yml file updated
    
    * workflow_dispatch added
    
    * updated mvc core
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml

--- test/Tests.cs
+++ test/Tests.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Json;
 using FluentAssertions;
 using Newtonsoft.Json;
+using infrastructure.datamodels;
 
 namespace testDir;
 
@@ -14,18 +15,19 @@ public class Tests
         _httpClient = new HttpClient();
     }
 
+    [TestCase("http://localhost:5081/TaxaApis/GetTaxaPrices/5,5,1", 125)]
+    [TestCase("http://localhost:5081/TaxaApis/GetTaxaPrices/8,10,1", 199)]
     public async Task Test1(string address, int expected)
     {
         //Arrange
-        //ACT
-        var answer = await _httpClient.GetStringAsync(address);
-        
-        //ASSERT
-        int.Parse(answer).Should().Be(expected);
-    }
-}
+        var response = await _httpClient.GetStringAsync(address);
+        var taxiDTOs = JsonConvert.DeserializeObject<TaxiDTO[]>(response);
 
-class Response
-{
-    public int? ExpectedResponse { get; set; }
+        //Act
+        var falseTaxi = Array.Find(taxiDTOs, taxi => taxi.CompanyName == "FalseTaxi");
+        
+        //Assert
+        Assert.NotNull(falseTaxi, "FalseTaxi not found in the response");
+        Assert.AreEqual(expected, falseTaxi.TaxiPrice, "Incorrect taxi price for FalseTaxi");
+    }
 }
