bd99f6626339acbbfd9be3bafa7ffba86933977b
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-12-15 11:15:13 +0100

    maps directions

--- frontend/src/app/maps/maps.component.ts
+++ frontend/src/app/maps/maps.component.ts
@@ -1,73 +1,89 @@
-import {Component, OnInit} from "@angular/core";
-import {GOOGLEAPIKEY} from "./apikey";
-import {firstValueFrom, Subscription} from "rxjs";
-import {HttpClient} from "@angular/common/http";
-import {State} from "src/state";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { GOOGLEAPIKEY } from "./apikey";
+import { firstValueFrom, Subscription } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { State } from "src/state";
+import { Destination, Position } from "src/models";
 
 @Component({
   template: `
-    <google-map *ngIf="apiLoaded"
-                height="400px"
-                width="750px"
-                [center]="center"
-                [zoom]="zoom">
-      <map-marker *ngFor="let position of markerPositions" [position]="position" [options]="markerOptions"/>
-    </google-map>`,
+    <google-map *ngIf="apiLoaded" height="400px" width="750px" [center]="center" [zoom]="zoom">
+      <map-marker *ngIf="pos" [position]="pos" [options]="markerOptions" />
+      <map-marker *ngIf="des" [position]="des" [options]="markerOptions" />
+      <map-directions
+        *ngIf="pos && des"
+        [origin]="pos"
+        [destination]="des"
+        [travelMode]='"DRIVING"'></map-directions>
+    </google-map>
+  `,
   selector: 'app-maps',
 })
-export class MapsComponent implements OnInit {
+export class MapsComponent implements OnInit, OnDestroy {
   private desMarkerSub: Subscription;
   private posMarkerSub: Subscription;
-  // prøv med at lægge Pos og Des herinde i stedet
+  pos = new Position();
+  des = new Destination();
   apiLoaded = false;
 
+  // Separate arrays for position and destination markers
+  posMarkerPositions: google.maps.LatLngLiteral[] = [];
+  desMarkerPositions: google.maps.LatLngLiteral[] = [];
+
   constructor(private httpClient: HttpClient, public state: State) {
-    this.loadMap()
-    this.posMarkerSub= this.state.posSub$.subscribe((value) => {
-      this.markerPositions = [];
-      this.addMarker(value)
-      if(this.state.des) {
-        this.addMarker(this.state.des)
-      }
-    });
-    this.desMarkerSub= this.state.desSub$.subscribe((value) => {
-      this.markerPositions = []
-      this.addMarker(value)
-      if(this.state.pos != null) {
-        this.addMarker(this.state.pos)
-      }
+    this.loadMap();
+
+    this.posMarkerSub = this.state.posSub$.subscribe((value) => {
+      this.pos = value;
+      this.updateMarkers();
     });
 
-
-
+    this.desMarkerSub = this.state.desSub$.subscribe((value) => {
+      this.des = value;
+      this.updateMarkers();
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.posMarkerSub.unsubscribe();
+    this.desMarkerSub.unsubscribe();
   }
 
   async loadMap() {
-    await firstValueFrom(this.httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=' + GOOGLEAPIKEY, 'callback'));
+    await firstValueFrom(
+      this.httpClient.jsonp(
+        'https://maps.googleapis.com/maps/api/js?key=' + GOOGLEAPIKEY + '&libraries=places,geometry,directions',
+        'callback'
+      )
+    );
     this.apiLoaded = true;
   }
 
-  center: google.maps.LatLngLiteral = {lat: 55.48773485445012, lng: 8.446890874949103}; //her er easv fx
+  center: google.maps.LatLngLiteral = {
+    lat: 55.48773485445012,
+    lng: 8.446890874949103,
+  };
   zoom = 16;
-  markerOptions: google.maps.MarkerOptions = {draggable: false};
-  markerPositions: google.maps.LatLngLiteral[] = [];
+  markerOptions: google.maps.MarkerOptions = { draggable: false };
 
-  addMarker(eventOrPosition: google.maps.LatLngLiteral) {
+  updateMarkers() {
+    // Clear existing markers
+    this.posMarkerPositions.pop();
+    this.desMarkerPositions.pop();
 
-    const position = eventOrPosition;
-    // Add the new marker
-    this.markerPositions.push(position);
-    console.log("Hey taxapp, der er nu en marker på kortet");
+    if (this.pos.lat != null) {
+      this.addMarker(this.posMarkerPositions, this.pos);
+    }
+
+    if (this.des.lat != null) {
+      this.addMarker(this.desMarkerPositions, this.des);
+    }
   }
 
-
-  removeMarker(remove: google.maps.LatLngLiteral) {
-    console.log('Removing marker:', remove);
-    this.markerPositions = this.markerPositions.filter(
-      (marker) => marker.lat !== remove.lat || marker.lng !== remove.lng
-    );
+  addMarker(markerPositions: google.maps.LatLngLiteral[], position: Position) {
+    const newPosition = { lat: position.lat!, lng: position.lng! };
+    markerPositions.push(newPosition);
   }
 }
