e8014902b6330ba7154c27d0a521554e8c7dbf6e
Ane Hoimark <anehoimark@gmail.com>
2023-12-20 17:33:38 +0100

    when logged in, email is set instantly

--- frontend/src/app/confirm-price/confirm-price.component.ts
+++ frontend/src/app/confirm-price/confirm-price.component.ts
@@ -5,8 +5,9 @@ import {DataContainer} from '../data.service'
 import {HttpClient} from "@angular/common/http";
 import {environment} from 'src/environments/environment';
 import {firstValueFrom} from "rxjs";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormsModule, Validators} from "@angular/forms";
 import {ModalController, ToastController} from "@ionic/angular";
+import { State } from 'src/state';
 
 @Component({
   selector: 'app-confirm-price',
@@ -19,13 +20,13 @@ import {ModalController, ToastController} from "@ionic/angular";
               <p>Duration: {{ dataContainer.data.min }} min</p>
               <p>Persons: {{ dataContainer.data.persons }}</p>
               <p>Price: {{ dataContainer.data.price }} DKK </p>
-              <ion-input label="Email" type="text" [(ngModel)]="emailOfUser" label-placement="floating" fill="outline"
-                         placeholder="Email">Email
+              <ion-input label="Email" labelPlacement="stacked" type="text" [formControl]="emailForm" [(ngModel)]="emailOfUser" fill="outline"
+                         placeholder="Enter Email">
               </ion-input>
-              <ion-input label="Phone Number" label-placement="floating" fill="outline" placeholder="Phone Number">Phone
+              <ion-input label="Phone Number" label-placement="stacked" fill="outline" placeholder="Enter Number">Phone
                   Number
               </ion-input>
-              <ion-button data-dismiss="modal" (click)="sendConfirmationEmail()">Confirm</ion-button>
+              <ion-button data-dismiss="modal" [disabled]="emailForm.invalid" (click)="sendConfirmationEmail()">Confirm</ion-button>
 
       </ion-content>
   `,
@@ -33,17 +34,22 @@ import {ModalController, ToastController} from "@ionic/angular";
 })
 export class ConfirmPriceComponent implements OnInit {
   emailOfUser: string | undefined;
+  emailForm = new FormControl('', [Validators.required, Validators.minLength(3)]);
+
 
   @Input()
   data!: ConfirmPriceDTO;
 
-
   dataContainer = inject(DataContainer);
-
-  constructor(private route: ActivatedRoute, public http: HttpClient, public toastcontroller: ToastController, public modalController: ModalController) {
+  constructor(private route: ActivatedRoute, public state: State, public http: HttpClient, public toastcontroller: ToastController, public modalController: ModalController) {
   }
 
   ngOnInit() {
+    if(this.state.getCurrentUser().email !== undefined){
+      this.emailOfUser = this.state.getCurrentUser().email;
+    }
+    else {
+    }
   }
 
   protected readonly ConfirmPriceDTO: any;
